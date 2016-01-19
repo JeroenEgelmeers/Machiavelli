7311de0f891a98b5c8ded4954106c2d342830363
Jeroen Egelmeers <mail@jeroenegelmeers.nl>
2016-01-19 15:46:08 +0100

    Removed numbers, not needed anymore as I added NONE (has to be added to be able to give each card a "CharacterType" value).

--- Machiavelli/EnumCard.h
+++ Machiavelli/EnumCard.h
@@ -1,4 +1,4 @@
 #pragma once
 // Warning; should have namespaces when not using C++ 11!  But as we use C++11+ we use the clean version. :)
 enum class CardColor		{ white, yellow, green, blue, red, lila };
-enum class CharacterType	{ NONE = 0, Moordenaar = 1, Dief = 2, Magiër = 3, Koning = 4, Prediker = 5, Koopman = 6, Bouwmeester = 7, Condotierre = 8 };
+enum class CharacterType	{ NONE, Moordenaar, Dief, Magiër, Koning, Prediker, Koopman, Bouwmeester, Condotierre };
