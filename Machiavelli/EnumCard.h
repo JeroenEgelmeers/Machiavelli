03edd7eabfca931cf7d0d30392129480123ca76c
Jeroen Egelmeers <mail@jeroenegelmeers.nl>
2016-01-19 14:19:17 +0100

    Fixed a typo

--- Machiavelli/EnumCard.h
+++ Machiavelli/EnumCard.h
@@ -1,4 +1,4 @@
 #pragma once
 // Warning; should have namespaces when not using C++ 11!  But as we use C++11+ we use the clean version. :)
-enum class CardColor		{ wite, yellow, green, blue, red, lila };
+enum class CardColor		{ white, yellow, green, blue, red, lila };
 enum class CharacterType	{ NONE = 0, Moordenaar = 1, Dief = 2, Magiër = 3, Koning = 4, Prediker = 5, Koopman = 6, Bouwmeester = 7, Condotierre = 8 };
