3db75e1ce172ecf4e671bfae86685c7a15a623ee
Jeroen Egelmeers <mail@jeroenegelmeers.nl>
2016-01-22 01:01:48 +0100

    CardDeck updated for only index numbers. Dief updated with information Game newRound done, Game SetupRound busy, Game pickCharacterCard busy

--- Machiavelli/Dief.cpp
+++ Machiavelli/Dief.cpp
@@ -8,6 +8,11 @@ Dief::~Dief(){}
 void Dief::Execute(Game game)
 {
 	game.getCurrentPlayer()->getClient()->write("You're playing the Thief.");
+	game.getCurrentPlayer()->getClient()->write("From which character would you like to steal gold?");
+	// Show options which chars are avaiable
+	// Wait on input
+	// Do input.
+
 	// TODO:: Say a character to steal gold from. [ Excl: murderer or killed character ]
 	
 }
